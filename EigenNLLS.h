7f80f9fe75fecce949f372702747bd45f1dde92b
Felix Laufer <felix.laufer@gmail.com>
2022-10-02 16:21:34 +0200

    Update EigenNLLS.h

--- EigenNLLS.h
+++ EigenNLLS.h
@@ -1,11 +1,11 @@
-#ifndef _NLLS_H_
-#define _NLLS_H_
+#ifndef _NLLS_FUNCTOR_H_
+#define _NLLS_FUNCTOR_H_
 
 #include "EigenTypes.h"
 #include <unsupported/Eigen/NonLinearOptimization>
 
 template<int NX = Eigen::Dynamic, int NY = Eigen::Dynamic>
-class NLLSObject
+class NLLSFunctor
 {
  public:
   enum
@@ -15,22 +15,31 @@ class NLLSObject
   };
 
   typedef ScalarType Scalar;
-  typedef Eigen::Matrix<ScalarType,InputsAtCompileTime,1> InputType;
-  typedef Eigen::Matrix<ScalarType,ValuesAtCompileTime,1> ValueType;
-  typedef Eigen::Matrix<ScalarType,ValuesAtCompileTime,InputsAtCompileTime> JacobianType;
+  typedef Eigen::Matrix<ScalarType, InputsAtCompileTime, 1> InputType;
+  typedef Eigen::Matrix<ScalarType, ValuesAtCompileTime, 1> ValueType;
+  typedef Eigen::Matrix<ScalarType, ValuesAtCompileTime, InputsAtCompileTime> JacobianType;
 
-  NLLSObject()
+  NLLSFunctor()
     : inputs_(InputsAtCompileTime), values_(ValuesAtCompileTime)
   {}
  
-  NLLSObject(unsigned int inputs, unsigned int values)
-    : inputs_(inputs), values_(values)
+  NLLSFunctor(unsigned int inputs, unsigned int values)
+    : inputs_(inputs)
+    , values_(values)
   {}
  
-  virtual ~NLLSObject() {}
+  virtual ~NLLSFunctor()
+  {}
 
-  unsigned int inputs() const { return inputs_; }
-  unsigned int values() const { return values_; }
+  unsigned int inputs() const
+  {
+    return inputs_;
+  }
+ 
+  unsigned int values() const
+  {
+    return values_;
+  }
  
   virtual int operator() (const InputType& x, ValueType& v) const = 0;
 
